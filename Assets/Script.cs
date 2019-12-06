892f51cf79223d7d012d07171157af32db3b7c50
Yanov-P <yanovpropeshtinktovich@gmail.com>
2019-12-06 18:43:10 +0300

    added Script

---
+++ Assets/Script.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("afawf");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
